284b7225e265671789cc104ea8154da5060af7d4
tarang1998 <tarangnair98@gmail.com>
2022-11-26 17:13:49 +0530

    update - DP prob : trapping rain water using two pointers

--- CompetitiveProgramming/DynamicProgramming/trappingRainWater.py
+++ CompetitiveProgramming/DynamicProgramming/trappingRainWater.py
@@ -1,5 +1,8 @@
 class Solution:
 
+    # Time Complexity : O(n)
+    # Space Complexity : O(n)
+
     def trap(self, height: List[int]) -> int:
 
         n = len(height)
@@ -29,6 +32,52 @@ class Solution:
 
         return totalAreaOfWater
             
+
+    
+
+    def trapWithTwoPointers(self, height: List[int]) -> int:
+
+        n = len(height)
+
+        maxLeftHeight = height[0]
+
+        maxRightHeight = height[n-1]
+
+        left = 1
+        right = n-2 
+
+        totalArea = 0 
+
+        while left<=right:
+
+            # Move towards the left side as water level would be dependent on the lesser height 
+            if(maxLeftHeight < maxRightHeight):
+
+                if(maxLeftHeight <= height[left]):
+                    maxLeftHeight = height[left]
+
+                else:
+                    totalArea += maxLeftHeight - height[left]
+                
+                left += 1 
+
+            # Move towards the right side 
+            else:
+
+                if(maxRightHeight <= height[right]):
+                    maxRightHeight = height[right]
+
+                else:
+                    totalArea += maxRightHeight - height[right]
+
+                right -= 1
+
+        return totalArea
+
+
+                
+
+               
 
 
                 
