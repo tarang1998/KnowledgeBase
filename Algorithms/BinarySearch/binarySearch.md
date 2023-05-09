a98cbad02708c5774e4ab52ea02ae0b70b2aa3f2
tarang1998 <tarangnair98@gmail.com>
2023-05-09 19:48:22 +0530

    Search a 2D matrix - binary search

--- Algorithms/BinarySearch/binarySearch.md
+++ Algorithms/BinarySearch/binarySearch.md
@@ -4,6 +4,11 @@
 
 ## Problems
 
+- Search a 2D Matrix  [[Problem Statement](https://leetcode.com/problems/search-a-2d-matrix/description/) | [Python Solution](/CompetitiveProgramming/BinarySearch//SearchA2DMatrix/searchA2DMAtrix.py)] <sub> (Diff : Medium, Topics : Array | Binary Search | Matrix)</sub> 
+
+
+---
+
 ### Single Element In Sorted Array
 
 - [Question](https://leetcode.com/problems/single-element-in-a-sorted-array/)
---
+++ CompetitiveProgramming/BinarySearch/SearchA2DMatrix/searchA2DMAtrix.py
@@ -0,0 +1,90 @@
+class Solution:
+
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        if(len(matrix) == 0):
+            return False
+
+        #Select the target row
+
+        rowCount = len(matrix)
+
+        columnCount = len(matrix[0])
+
+        low = 0 
+
+        high = rowCount - 1
+
+
+        while low <= high:
+
+            mid = (low + high)//2
+
+            if(matrix[mid][0] == target):
+
+                return True
+
+            if(matrix[mid][0] > target):
+
+                high = mid - 1
+            
+            else:
+
+                low = mid + 1
+
+        if(high < 0):
+            return False 
+
+        targetRow = high
+
+
+        #Search for the target in the target row 
+
+        left = 0
+
+        right = columnCount-1
+
+
+        while left <= right :
+
+            mid = (left + right)//2
+
+            if(matrix[targetRow][mid] == target):
+
+                return True
+            
+            if(matrix[targetRow][mid] > target):
+
+                right = mid - 1
+            
+            else:
+
+                left = mid + 1
+
+            
+        return False
+
+
+
+
+
+
+            
+
+
+    def searchMatrixBruteForce(self, matrix: List[List[int]], target: int) -> bool:
+
+        result = False
+
+        for l in matrix :
+
+            if target in l:
+
+                return True
+
+            if  target < l[-1]:
+
+                return False
+
+        return result
+
