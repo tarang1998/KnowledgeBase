7f546c3b79e8bb98fb0afe3621077a85bca63a42
tarang1998 <tarangnair98@gmail.com>
2024-01-05 22:38:19 -0500

    update - palindrome linked list

--- CompetitiveProgramming/LinkedList/palindromeLinkedList.py
+++ CompetitiveProgramming/LinkedList/palindromeLinkedList.py
@@ -1,5 +1,3 @@
-# https://leetcode.com/problems/palindrome-linked-list/
-
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -7,8 +5,46 @@
 #         self.next = next
 
 class Solution:
+
+    # Time Complexity : O(n)
+    # Space Complexity : O(1) 
+    def isPalindrome(self, head: ListNode) -> bool:
+        slow = head
+        fast = head 
+
+        # Find the middle element of the linked list 
+        while fast and fast.next:
+            slow = slow.next 
+            fast = fast.next.next
+
+        # Reverse the second half of the linked list to point to slow 
+        prev = slow
+        slow = slow.next
+        # Break the cycle to prevent an endless loop with the second half 
+        prev.next = None
+       
+        while(slow):
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+
+        # Compare the first half and the reversed second half 
+        while prev:
+            if prev.val != head.val:
+                return False
+            
+            head = head.next 
+            prev = prev.next
+
+        return True 
+
     
-    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        
+    
+    # Time Complexity : O(n)
+    # Space Complexity : O(1)
+    def isPalindrome1(self, head: Optional[ListNode]) -> bool:
         
         ele = []
         
@@ -21,8 +57,5 @@ class Solution:
         else: 
             return False
 
-        
-        
-        
-        
+   
         
