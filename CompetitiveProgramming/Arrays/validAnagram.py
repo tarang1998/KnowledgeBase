e04a006078bb709dbd1b503c1d30c7c6c94e7e25
tarang1998 <tarangnair98@gmail.com>
2024-01-05 22:47:30 -0500

    update - valid anagrams

--- CompetitiveProgramming/Arrays/validAnagram.py
+++ CompetitiveProgramming/Arrays/validAnagram.py
@@ -1,13 +1,32 @@
 class Solution:
     
-    # Time Complexity : O(m+n)
+    # Time Complexity : O(n)
+    # Space Complexity : O(1)
     def isAnagram(self, s: str, t: str) -> bool:
+
+        if(len(s) != len(t)):
+            return False
+
+        mem = [0] * 26 
+
+        for i in range(len(s)):
+            mem[ord(s[i]) - ord('a')] += 1
+            mem[ord(t[i]) - ord('a')] -= 1  
+        
+        return all(count == 0 for count in mem)
+
+
+    
+    # Time Complexity : O(n)
+    # Space Complexity : O(1)
+    def isAnagram1(self, s: str, t: str) -> bool:
         
         if(len(s) != len(t)):
             return False
     
         wordCount = {}
         
+        # Fill the map with all characters of the first string
         for i in range(len(s)):
             
             if(s[i] not in wordCount):
@@ -16,7 +35,8 @@ class Solution:
                 
             else:
                 wordCount[s[i]] +=1
-                
+        
+        # Check if characters in the second string is present in the first string 
         for j in range(len(t)):
             
             if(t[j] not in wordCount):
