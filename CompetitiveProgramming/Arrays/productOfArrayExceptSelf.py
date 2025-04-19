68f993ea44071f86267a8844283a3800d3ea3bbc
Tarang <tarangnair98@gmail.com>
2025-04-19 17:17:34 -0400

    Product of array except self

--- CompetitiveProgramming/Arrays/productOfArrayExceptSelf.py
+++ CompetitiveProgramming/Arrays/productOfArrayExceptSelf.py
@@ -24,29 +24,4 @@ class Solution:
             suffix *= num
 
         return output
-class Solution:
-    # Time Complexity : O(n)
-    # Space Complexity : O(n)
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
 
-        output = []
-
-        prefix = 1
-
-        # Looping from the beginning of the array to the end
-        # computing the product of all the integers before a certain position  
-
-        for num in nums:
-            output.append(prefix)
-            prefix *= num 
-
-        suffix = 1
-
-        # Looping from the end of the array to the beginning 
-        # computing the product of all the integers after a certain position
-        for i in range(len(nums)-1,-1,-1):
-            num = nums[i]
-            output[i] *= suffix
-            suffix *= num
-
-        return output
