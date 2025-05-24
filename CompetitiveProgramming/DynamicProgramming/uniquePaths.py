3e2f0a299f70836583ec888e7a4d1872b53df887
Tarang <tarangnair98@gmail.com>
2025-05-24 03:44:46 -0400

    DP - Unique Paths

--- CompetitiveProgramming/DynamicProgramming/uniquePaths.py
+++ CompetitiveProgramming/DynamicProgramming/uniquePaths.py
@@ -1,32 +1,46 @@
 class Solution:
 
-    
+    # Time Complexity : O(n*m)
+    # Space Comlexity : O(n)
     def uniquePaths(self, m: int, n: int) -> int:
 
-        #All the elements in the last row would have one way to reach the destination
+        # Each element in the last row would have only one way to reach the finish point
         row = [1] * n
 
-        #Loop through m-1 rows from bottom up 
-        for i in range(m-1):
+        for i in range(m-2,-1,-1):
 
+            # Computing unique paths for the row above
             temp = [1] * n
 
-            # The last element of each row would also have only one way to reach to the destination
             for j in range(n-2,-1,-1):
-
-                # The no of paths to reach the destination is equal to the
-                # no of paths from the right element + no of paths from the bottom element
                 temp[j] = temp[j+1] + row[j]
 
             row = temp
 
-        # Returns the number of unique paths required to traverse from grid[0][0] to grid[m-1][n-1]
         return row[0]
 
 
+    def uniquePaths2(self, m: int, n: int) -> int:
 
-        
-      
-        
-        
+        mem = {}
+
+        def traverse(m,n):
+
+            if (m,n) in mem:
+                return mem[(m,n)]
+
+            if m == 1 and n == 1:
+                return 1 
+
+            if m < 1:
+                return 0 
+            
+            if n < 1:
+                return 0
+
+            mem[(m,n)] =  traverse(m-1,n) + traverse(m,n-1)
+            return mem[(m,n)]
+
+        return traverse(m,n)
+
         
