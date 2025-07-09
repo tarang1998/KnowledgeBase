cc41a309f1384dab9f68fcbffeaf78b34fd19df1
Tarang <tarangnair98@gmail.com>
2025-07-09 03:21:09 -0400

    Greedy : Jump Game

--- CompetitiveProgramming/GreedyAlgorithms/jumpGame2.py
+++ CompetitiveProgramming/GreedyAlgorithms/jumpGame2.py
@@ -1,74 +1,31 @@
+from typing import List
+
+
 class Solution:
-
-    # Greedy Approach
-    def jump(self,nums : List[int]) -> int:
-
-        # Maintain two pointers that indicate the
-        # left and the right boundary of the indexes that could be reached 
-        # after every iteration
-        l = r = 0 
-
-        # Create a farthest pointer to indicate the fathest index that could be 
-        # reached from a given set of indices
+    def jump(self, nums: List[int]) -> int:
+        # Time Complexity : O(n)
+        # Initialize variables:
+        # jumps: number of jumps made so far
+        # current_end: farthest index we can reach with current number of jumps
+        # farthest: farthest index we can reach in the current iteration
+        jumps = 0
+        current_end = 0
         farthest = 0
 
-        steps = 0 
+        # We iterate up to the second-last element because we don't need to jump *from* the last index
+        for i in range(len(nums) - 1):
+            # Update the farthest we can reach from index i
+            farthest = max(farthest, i + nums[i])
 
-        while(r < len(nums)-1):
+            # If we have reached the end of the current jump's range,
+            # we must make another jump to continue
+            if i == current_end:
+                jumps += 1
+                current_end = farthest  # Move to the new farthest boundary
 
-            # Loop through the given set borderedby l and r 
-            # to find farthest index that could be reached
-            for i in range(l,r+1):
+                # Optional early exit if we've already reached the end
+                if current_end >= len(nums) - 1:
+                    break
 
-                farthest = max(farthest, i + nums[i])
+        return jumps
 
-            l = r + 1
-
-            r = farthest
-
-            steps += 1
-
-        return steps
-
-                
-
-
-
-
-
-    # Dynamic Programming Recursive Solution
-    def jump1(self, nums: List[int]) -> int:
-
-        # Storing the no of steps required to reach the destination from the index
-        # If dest cant be reaced a value of infinity is assigned 
-        mem = {}
-
-        def dfs(index):
-
-            # If index has reached the final destination
-            if(index == len(nums)-1):
-                return 0
-
-            # If index goes beyond the final destination
-            if(index >= len(nums)):
-                return float('inf')
-
-            # If the value for this index is already computed 
-            if(index in mem):
-                return mem[index]
-
-            ele = nums[index]
-
-            minSteps = float('inf')
-
-            for i in range( 1 , ele + 1):
-
-                minSteps = min(minSteps,1 + dfs(index + i))
-
-            mem[index] = minSteps
-
-            return mem[index]
-
-
-        return dfs(0)
-        
